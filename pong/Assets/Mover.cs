51888bf193a46fbfb9ace3c3b5d2f9d65a9a4b57
[ccclend] <clendechy@csumb.edu>
2022-02-06 21:11:25 -0800

    ...

--- pong/Assets/Mover.cs
+++ pong/Assets/Mover.cs
@@ -1,57 +1,61 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class Mover : MonoBehaviour
 {
-    public float movementPerSecond = 1f;
+    public float movementPerSecond = 10f;
+    public string paddleName;
 
+    private Rigidbody _rbody;
     // Start is called before the first frame update
     void Start()
     {
-
+        _rbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float movementAxis = Input.GetAxis("Horizontal");
-        // Transform transform = GetComponent<Transform>();
-
-        Vector3 force = Vector3.right * movementAxis * movementPerSecond * Time.deltaTime;
-
-        Rigidbody rbody = GetComponent<Rigidbody>();
-        if (rbody)
+        
+        float movementAxis;
+        if (paddleName == "leftPaddle")
         {
-            rbody.AddForce(force, ForceMode.VelocityChange);
+             movementAxis = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            movementAxis = Input.GetAxis("Vertical2");
         }
 
-        // transform.position += Vector3.right * movementAxis * movementPerSecond * Time.deltaTime;
+        
+        
+        
+        Vector3 force = Vector3.up * movementAxis * movementPerSecond * Time.deltaTime;
 
-        // if (Input.GetKey(KeyCode.A))
-        // {
-        //     Transform transform = GetComponent<Transform>();
-        //     transform.position += -Vector3.right * movementPerSecond * Time.deltaTime;
-        // }
-        //
-        // if (Input.GetKey(KeyCode.D))
-        // {
-        //     Transform transform = GetComponent<Transform>();
-        //     transform.position += Vector3.right * movementPerSecond * Time.deltaTime;
-        // }
+        _rbody.AddForce(force, ForceMode.VelocityChange);
+        
+
+
     }
-
+    
     private void OnCollisionEnter(Collision collision)
     {
-        BoxCollider bbox = GetComponent<BoxCollider>();
-        float xCenter = bbox.bounds.center.x;
+        
+        // BoxCollider bbox = GetComponent<BoxCollider>();
+        // float xCenter = bbox.bounds.center.x;
+        //
+        // Debug.Log("Center at " + xCenter + "collided object at " + collision.transform.position.x);
 
-        Debug.Log("Center at " + xCenter + "collided object at " + collision.transform.position.x);
-
-        Vector3 newVector = Quaternion.Euler(0f, 0f, 45f) * Vector3.up;
-
-        Debug.DrawLine(transform.position, transform.position + newVector * 10f, Color.red);
-        Debug.Break();
+        // Vector3 newVector = Quaternion.Euler(0f, 0f, 45f) * Vector3.up;
+        
+        // Debug.DrawLine(transform.position, transform.position + newVector * 10f, Color.red);
+        
+        // Debug.Break();
     }
+
+ 
 }
